e4f6592ced395d217317eb5a3d180d8c13596781
Guilherme-Cervantes <122274829+Guilherme-Cervantes@users.noreply.github.com>
2024-09-09 15:26:51 -0300

    Update pkg_vinculacao.dart

--- lib/pkg_vinculacao.dart
+++ lib/pkg_vinculacao.dart
@@ -3,3 +3,4 @@ library pkg_vinculacao;
 export 'src/pages/vinculacao/vinculacao_page.dart';
 export 'src/models/codigo_vinculacao/codigo_vinculacao.dart';
 export 'src/models/dados_aplicativo/dados_aplicativo.dart';
+export 'src/enums/aplicativo_enum.dart';
