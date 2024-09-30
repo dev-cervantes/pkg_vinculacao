02048be0ad8be191fb7ecdf901c19e7c8800ecb3
Guilherme-Cervantes <122274829+Guilherme-Cervantes@users.noreply.github.com>
2024-09-30 14:07:36 -0300

    Realizado ajustes de tela.
    * Salvar ao apertar enter.
    * Melhorado o CircularProgressIndicator.
    * Realizado melhorias da tela: aumentado o botão de enviar/vincular, diminuido o campo de enviar cpf/cnpj

--- lib/src/pages/vinculacao/vinculacao_page.dart
+++ lib/src/pages/vinculacao/vinculacao_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:linear_timer/linear_timer.dart';
 import 'package:pkg_flutter_utils/masked.dart';
 import 'package:pkg_flutter_utils/validators.dart';
@@ -6,12 +7,19 @@ import 'package:pkg_vinculacao/src/controllers/vinculacao/vinculacao_controller.
 import 'package:pkg_vinculacao/src/models/dados_aplicativo/dados_aplicativo.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:shimmer/shimmer.dart';
+import 'dart:async';
 
 class VinculacaoPage extends StatefulWidget {
   final nomeAplicativo;
   final Function(DadosAplicativo dadosAplicativo) onVinculado;
+  final Function(BuildContext, Exception, StackTrace) onCodigoNaoEncontrado;
 
-  const VinculacaoPage({super.key, required this.nomeAplicativo, required this.onVinculado});
+  const VinculacaoPage({
+    super.key,
+    required this.nomeAplicativo,
+    required this.onVinculado,
+    required this.onCodigoNaoEncontrado,
+  });
 
   @override
   State<VinculacaoPage> createState() => _VinculacaoPageState();
@@ -21,151 +29,203 @@ class _VinculacaoPageState extends State<VinculacaoPage> with TickerProviderStat
   final VinculacaoController _controller = VinculacaoController();
   final TextEditingController _cpfCnpjController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FocusNode _focus = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              theme.colorScheme.primary,
-              theme.colorScheme.secondary,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const FlutterLogo(size: 200),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Text(
-                    widget.nomeAplicativo,
-                    style: theme.textTheme.titleLarge?.copyWith(color: Colors.white),
-                  )
-                ],
-              ),
+    return KeyboardListener(
+      autofocus: true,
+      focusNode: _focus,
+      onKeyEvent: (event) async {
+        await _save();
+      },
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                theme.colorScheme.primary,
+                theme.colorScheme.secondary,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            Expanded(
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ValueListenableBuilder(
-                    valueListenable: _controller.etapas,
-                    builder: (_, value, __) {
-                      if (value == 1) {
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Digite o CPF ou CNPJ para realizar a vinculação com o aplicativo",
-                              style: theme.textTheme.titleMedium?.copyWith(
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 32,
-                            ),
-                            Form(
-                              key: _formKey,
-                              child: TextFormField(
-                                controller: _cpfCnpjController,
-                                decoration: InputDecoration(
-                                    labelText: '* CPF/CNPJ',
-                                    labelStyle: TextStyle(
-                                      color: theme.colorScheme.primary,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                    hintText: 'Digite o CPF ou o CNPJ'),
-                                inputFormatters: [InputMasked.cnpjCpf()],
-                                keyboardType: TextInputType.number,
-                                validator: InputValidator([CnpjCpfValidator()], isRequired: false).validate,
-                                onSaved: (value) {
-                                  _controller.cpfCnpj = value!;
-                                },
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 16,
-                            ),
-                            FilledButton(
-                              onPressed: () {
-                                if (_formKey.currentState?.validate() ?? false) {
-                                  _formKey.currentState?.save();
-                                  if (_controller.cpfCnpj.isNotEmpty) {
-                                    _controller.enviarCpfCnpj(_controller.cpfCnpj, widget.onVinculado);
-                                  }
-                                }
-                              },
-                              child: const Text('ENVIAR'),
-                            ),
-                          ],
-                        );
-                      }
-                      return ValueListenableBuilder(
-                        valueListenable: _controller.codigo,
-                        builder: (_, value, __) {
-                          if (value.isEmpty) return const CircularProgressIndicator();
-                          return Row(
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const FlutterLogo(size: 200),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      widget.nomeAplicativo,
+                      style: theme.textTheme.titleLarge?.copyWith(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 122.0),
+                    child: ValueListenableBuilder(
+                      valueListenable: _controller.etapas,
+                      builder: (_, value, __) {
+                        if (value == 1) {
+                          return Column(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              QrImageView(
-                                data: value,
-                                size: 150,
+                              Text(
+                                "Olá! ",
+                                style: theme.textTheme.titleLarge?.copyWith(
+                                  fontWeight: FontWeight.w700,
+                                ),
                               ),
-                              const SizedBox(width: 16),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Código de vinculação',
-                                    style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w500, fontSize: 30),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Text(
+                                "Realize a vinculação com o aplicativo",
+                                style: theme.textTheme.titleMedium?.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 32,
+                              ),
+                              Text(
+                                'CPF/CNPJ',
+                                style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              Form(
+                                key: _formKey,
+                                child: SizedBox(
+                                  width: 400,
+                                  child: TextFormField(
+                                    controller: _cpfCnpjController,
+                                    decoration: const InputDecoration(hintText: 'Digite o CPF ou CNPJ'),
+                                    inputFormatters: [InputMasked.cnpjCpf()],
+                                    keyboardType: TextInputType.number,
+                                    validator: InputValidator([CnpjCpfValidator()], isRequired: false).validate,
+                                    onSaved: (value) {
+                                      _controller.cpfCnpj = value!;
+                                    },
                                   ),
-                                  Text(
-                                    value,
-                                    style: theme.textTheme.titleLarge?.copyWith(color: theme.colorScheme.secondary, fontWeight: FontWeight.bold, fontSize: 60),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 24,
+                              ),
+                              Center(
+                                child: SizedBox(
+                                  width: 200,
+                                  child: FilledButton(
+                                    onPressed: () async {
+                                    await _save();
+                                    },
+                                    child: const Text('Vincular'),
                                   ),
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(8),
-                                    child: SizedBox(
-                                      width: 200,
-                                      height: 10,
-                                      child: LinearTimer(
-                                        key: UniqueKey(),
-                                        duration: Duration(seconds: _controller.dataExpiracao.value.difference(DateTime.now()).inSeconds),
-                                        color: theme.colorScheme.primary,
-                                        backgroundColor: theme.colorScheme.primary.withOpacity(0.3),
-                                        forward: false,
-                                        onTimerEnd: () {
-                                        _controller.gerarNovoCodigo(widget.onVinculado);
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                                ),
                               ),
                             ],
                           );
-                        },
-                      );
-                    },
+                        }
+
+                        return ValueListenableBuilder(
+                          valueListenable: _controller.codigo,
+                          builder: (_, value, __) {
+                            if (value.isEmpty) {
+                              return const Center(
+                                child: SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: CircularProgressIndicator(),
+                                ),
+                              );
+                            }
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                QrImageView(
+                                  data: value,
+                                  size: 150,
+                                ),
+                                const SizedBox(width: 16),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Código de vinculação',
+                                      style: theme.textTheme.titleLarge?.copyWith(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 30,
+                                      ),
+                                    ),
+                                    Text(
+                                      value,
+                                      style: theme.textTheme.titleLarge?.copyWith(
+                                        color: theme.colorScheme.secondary,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 60,
+                                      ),
+                                    ),
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(8),
+                                      child: SizedBox(
+                                        width: 200,
+                                        height: 10,
+                                        child: LinearTimer(
+                                          key: UniqueKey(),
+                                          duration: Duration(seconds: _controller.dataExpiracao.value.difference(DateTime.now()).inSeconds),
+                                          color: theme.colorScheme.primary,
+                                          backgroundColor: theme.colorScheme.primary.withOpacity(0.3),
+                                          forward: false,
+                                          onTimerEnd: () {
+                                            _controller.gerarNovoCodigo(widget.onVinculado);
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _save() async {
+    try {
+      if (_formKey.currentState?.validate() ?? false) {
+        _formKey.currentState?.save();
+        if (_controller.cpfCnpj.isNotEmpty) {
+          await _controller.enviarCpfCnpj(_controller.cpfCnpj, widget.onVinculado);
+        }
+      }
+    } catch (e, s) {
+      widget.onCodigoNaoEncontrado(context, e as Exception, s);
+      _controller.etapas.value = 1;
+    }
   }
 }
