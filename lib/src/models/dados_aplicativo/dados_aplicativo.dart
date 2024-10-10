998d6c5f6996395059899300d04f56290d009a81
luciano-cervantes <72446530+luciano-cervantes@users.noreply.github.com>
2024-10-10 08:42:47 -0300

    Add idCliente

--- lib/src/models/dados_aplicativo/dados_aplicativo.dart
+++ lib/src/models/dados_aplicativo/dados_aplicativo.dart
@@ -4,6 +4,7 @@ class DadosAplicativo {
   final String idVinculacaoAplicativo;
   final String cpfCnpj;
   final String nomeEmpresa;
+  final int idCliente;
   final String? host;
   final String? porta;
   final AplicativoEnum aplicativo;
@@ -13,6 +14,7 @@ class DadosAplicativo {
     required this.idVinculacaoAplicativo,
     required this.cpfCnpj,
     required this.nomeEmpresa,
+    required this.idCliente,
     required this.aplicativo,
     required this.host,
     required this.porta,
@@ -23,6 +25,7 @@ class DadosAplicativo {
         idVinculacaoAplicativo: json["IdVinculacaoAplicativo"],
         cpfCnpj: json["CpfCnpj"],
         nomeEmpresa: json["NomeEmpresa"],
+        idCliente: json["IdCliente"],
         aplicativo: AplicativoEnum.getByValue(json["Aplicativo"]),
         host: json["Host"],
         porta: json["Porta"],
